ca75538f69542133e565ae55f9059d9eaa5a5c46
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-03-25 14:40:58 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ['react', 'react-native'],
+  plugins: ['react', 'react-native', 'react-hooks'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -8,5 +8,20 @@ module.exports = {
   env: {
     'react-native/react-native': true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    // TODO: enable after https://github.com/import-js/eslint-plugin-import/issues/2348 performance downgrade is fixed
+    'import/no-cycle': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+  globals: {
+    fetch: false,
+  },
 }
