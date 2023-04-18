92ec0c224c16b27bc40273d97fb36b8a3d0c7b48
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-04-18 20:09:33 +0300

    Fix ESlint warnings in second tab

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import React from 'react'
 
 import { Text, View } from '../../components/Themed'
 
@@ -14,20 +15,20 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
   },
   description: {
     fontSize: 14,
   },
   separator: {
-    marginVertical: 30,
     height: 1,
+    marginVertical: 30,
     width: '80%',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 })
