97c90367ecf5174d16990d4c31b6b9d3c55ea704
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-05-18 22:55:20 +0300

    Add lobby screen styles

--- app/(tabs)/(chat)/lobby.tsx
+++ app/(tabs)/(chat)/lobby.tsx
@@ -1,12 +1,26 @@
+/* eslint-disable react-native/no-color-literals */
 import { useRouter } from 'expo-router'
 import React, { useState } from 'react'
-import { Button, StyleSheet, TextInput, Text, View, SafeAreaView } from 'react-native'
+import {
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native'
 
 import { Route } from '../../../constants/routes'
+
+import backImage from '../../../assets/images/backImage.png'
 
 export default function LobbyScreen() {
   const [userName, setUserName] = useState('')
   const [roomName, setRoomName] = useState('')
+
+  const isSubmitDisabled = !userName || !roomName
 
   const router = useRouter()
 
@@ -35,29 +49,89 @@ export default function LobbyScreen() {
 
   /* TODO implement <Weather /> */
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Username</Text>
+    <View style={styles.container}>
+      <Image source={backImage} style={styles.backImage} />
+      <View style={styles.whiteSheet} />
+      <SafeAreaView style={styles.form}>
+        <Text style={styles.title}>Chatter</Text>
         <TextInput
+          style={styles.input}
           value={userName}
           placeholder="Enter username"
           onChangeText={handleUserNameChange}
+          autoCapitalize="none"
+          autoFocus={true}
+          textContentType="username"
         />
-      </View>
-      <View>
-        <Text>Room</Text>
-        <TextInput value={roomName} placeholder="Enter room" onChangeText={handleRoomNameChange} />
-      </View>
-      <Button disabled={!userName || !roomName} title="Join" onPress={handleSubmit} />
-    </SafeAreaView>
+        <TextInput
+          style={styles.input}
+          value={roomName}
+          placeholder="Enter room"
+          onChangeText={handleRoomNameChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit}
+          activeOpacity={0.6}
+          disabled={isSubmitDisabled}
+        >
+          <Text style={{ fontWeight: 'bold', color: '#fff', fontSize: 18 }}>Join</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <StatusBar barStyle="light-content" />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backImage: {
+    height: 340,
+    position: 'absolute',
+    resizeMode: 'cover',
+    top: 0,
+    width: '100%',
+  },
+  button: {
     alignItems: 'center',
-    flex: 1,
-    gap: 10,
+    backgroundColor: '#f57c00',
+    borderRadius: 10,
+    height: 58,
     justifyContent: 'center',
+    marginTop: 40,
+  },
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 30,
+  },
+  input: {
+    backgroundColor: '#F6F7FB',
+    borderRadius: 10,
+    fontSize: 16,
+    height: 58,
+    marginBottom: 20,
+    padding: 12,
+  },
+  title: {
+    alignSelf: 'center',
+    color: 'orange',
+    fontSize: 36,
+    fontWeight: 'bold',
+    paddingBottom: 24,
+  },
+  whiteSheet: {
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 60,
+    bottom: 0,
+    height: '75%',
+    position: 'absolute',
+    width: '100%',
   },
 })
