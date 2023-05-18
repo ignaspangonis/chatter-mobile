dac97e3dc5521b84b46330f7e6915fe890c7ff40
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-05-19 01:11:06 +0300

    Add SendMessage deprecating comment

--- components/SendMessage/SendMessage.tsx
+++ components/SendMessage/SendMessage.tsx
@@ -5,6 +5,12 @@ type Props = {
   onSubmit: (message: string) => void
 }
 
+/**
+ * @description This is a component that allows the user to send a message to the chat
+ * @deprecated This component is deprecated and will be removed
+ * @param param0
+ * @returns
+ */
 export default function SendMessage({ onSubmit }: Props) {
   const [message, setMessage] = useState('')
 
